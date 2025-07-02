510055f274733aa22af5f2fa65253ee0d473656b
chingizpro <chingizprog@gmail.com>
2025-07-02 14:53:43 +0500

    Make footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,43 +3,51 @@ import logoSrc from "../assets/logo.png";
 const Footer = () => {
   return (
     <footer className="footer">
-        <div className="container">
-            <div className="footer__inner">
-                <a href="#" className="footer__logo">
-                    <img src={logoSrc} alt="footer logo" className="footer__pic" />
+      <div className="container">
+        <div className="footer__inner">
+          <a href="#" className="footer__logo">
+            <img src={logoSrc} alt="footer logo" className="footer__pic" />
+          </a>
+          <div className="footer__right">
+            <div className="footer__part">
+              <h2 className="footer__title">Navigation</h2>
+              <div className="footer__navigation">
+                <a href="#" className="footer__link">
+                  About
                 </a>
-                <div className="footer__right">
-                    <div className="footer__part">
-                        <h2 className="footer__title">Navigation</h2>
-                        <div className="footer__navigation">
-                            <a href="#" className="footer__link">About</a>
-                            <a href="#" className="footer__link">Groups</a>
-                            <a href="#" className="footer__link">Teachers</a>
-                            <a href="#" className="footer__link">Contact</a>
-                        </div>
-                    </div>
-                    <div className="footer__part">
-                        <h2 className="footer__title">Social media</h2>
-                        <div className="footer__social">
-                            <a href="#" className="footer__link footer__link--social">
-                                <i className="footer__icon fa-brands fa-telegram"></i>
-                                <span className="footer__social-text">Telegram</span>
-                            </a>
-                            <a href="#" className="footer__link footer__link--social">
-                                <i className="footer__icon fa-brands fa-instagram"></i>
-                                <span className="footer__social-text">Instagram</span>
-                            </a>
-                            <a href="#" className="footer__link footer__link--social">
-                                <i className="footer__icon fa-brands fa-youtube"></i>
-                                <span className="footer__social-text">YouTube</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                <a href="#" className="footer__link">
+                  Groups
+                </a>
+                <a href="#" className="footer__link">
+                  Teachers
+                </a>
+                <a href="#" className="footer__link">
+                  Contact
+                </a>
+              </div>
             </div>
+            <div className="footer__part">
+              <h2 className="footer__title">Social media</h2>
+              <div className="footer__social">
+                <a href="#" className="footer__link footer__link--social">
+                  <i className="footer__icon fa-brands fa-telegram"></i>
+                  <span className="footer__social-text">Telegram</span>
+                </a>
+                <a href="#" className="footer__link footer__link--social">
+                  <i className="footer__icon fa-brands fa-instagram"></i>
+                  <span className="footer__social-text">Instagram</span>
+                </a>
+                <a href="#" className="footer__link footer__link--social">
+                  <i className="footer__icon fa-brands fa-youtube"></i>
+                  <span className="footer__social-text">YouTube</span>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
