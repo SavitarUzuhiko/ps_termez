243322c736893fdd88d59846ec050e285e753d22
chingizpro <chingizprog@gmail.com>
2025-07-03 20:41:19 +0500

    Change main section design

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,12 +1,18 @@
-import sliderImageSource from "../assets/slider-photo.jpg"
+import sliderImageSource from "../assets/slider-photo.jpg";
+import { BsArrowRightCircle } from "react-icons/bs";
 
 const Slide = () => {
   return (
     <div className="slide">
       <div className="slide__content">
         <h2 className="slide__title">Slide title</h2>
-        <p className="slide__lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor, laudantium.</p>
-        <a href="#" className="slide__link">Learn more</a>
+        <p className="slide__lead">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor,
+          laudantium.
+        </p>
+        <a href="#" className="slide__link">
+          More <BsArrowRightCircle className="slide__link-icon" />
+        </a>
       </div>
       <img src={sliderImageSource} alt="slide photo" className="slide__image" />
     </div>
