d7a7b655d5faca20933d1e49f65273e3ee5fb114
chingizpro <chingizprog@gmail.com>
2025-07-28 12:03:13 +0500

    Restyle and remake Team section

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,71 +1,36 @@
 import source from "../assets/teacher_avatar.jpg";
+import teamData from "../data/teamData";
 
 const Team = () => {
   return (
     <section className="team">
-        <div className="container">
+      <div className="container">
         <div className="team__textbox">
-            <h2 className="team__title">Meet our team</h2>
-            <p className="team__subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam ab laudantium quae quos perspiciatis iure nesciunt quod.</p>
-            <a href="#" className="team__more">All teachers</a>
+          <h2 className="team__title">Meet our team</h2>
+          <p className="team__subtitle">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam ab
+            laudantium quae quos perspiciatis iure nesciunt quod.
+          </p>
+          <a href="#" className="team__more">
+            All teachers
+          </a>
         </div>
+      </div>
+      <div className="members">
+        <div className="members__inner">
+          {teamData.map((m) => (
+            <article className="member">
+              <img src={m.image} alt="team member" className="member__image" />
+              <div className="member__box">
+                <h3 className="member__name">{m.name}</h3>
+                <p className="member__info">{m.role}</p>
+              </div>
+            </article>
+          ))}
         </div>
-        <div className="members">
-            <div className="members__inner">
-                <article className="member">
-                    <img src={source} alt="team member" className="member__image" />
-                    <h3 className="member__name">Member name</h3>
-                    <p className="member__info">Lorem ipsum dolor</p>
-                </article>
-                <article className="member">
-                    <img src={source} alt="team member" className="member__image" />
-                    <h3 className="member__name">Member name</h3>
-                    <p className="member__info">Lorem ipsum dolor</p>
-                </article>
-                <article className="member">
-                    <img src={source} alt="team member" className="member__image" />
-                    <h3 className="member__name">Member name</h3>
-                    <p className="member__info">Lorem ipsum dolor</p>
-                </article>
-                <article className="member">
-                    <img src={source} alt="team member" className="member__image" />
-                    <h3 className="member__name">Member name</h3>
-                    <p className="member__info">Lorem ipsum dolor</p>
-                </article>
-                <article className="member">
-                    <img src={source} alt="team member" className="member__image" />
-                    <h3 className="member__name">Member name</h3>
-                    <p className="member__info">Lorem ipsum dolor</p>
-                </article>
-                <article className="member">
-                    <img src={source} alt="team member" className="member__image" />
-                    <h3 className="member__name">Member name</h3>
-                    <p className="member__info">Lorem ipsum dolor</p>
-                </article>
-                <article className="member">
-                    <img src={source} alt="team member" className="member__image" />
-                    <h3 className="member__name">Member name</h3>
-                    <p className="member__info">Lorem ipsum dolor</p>
-                </article>
-                <article className="member">
-                    <img src={source} alt="team member" className="member__image" />
-                    <h3 className="member__name">Member name</h3>
-                    <p className="member__info">Lorem ipsum dolor</p>
-                </article>
-                <article className="member">
-                    <img src={source} alt="team member" className="member__image" />
-                    <h3 className="member__name">Member name</h3>
-                    <p className="member__info">Lorem ipsum dolor</p>
-                </article>
-                <article className="member">
-                    <img src={source} alt="team member" className="member__image" />
-                    <h3 className="member__name">Member name</h3>
-                    <p className="member__info">Lorem ipsum dolor</p>
-                </article>
-            </div>
-        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Team
+export default Team;
---
+++ src/data/teamData.ts
@@ -0,0 +1,17 @@
+import source1 from "../assets/team_man.jpg";
+import source2 from "../assets/team_woman.jpg";
+
+const teamData = [
+  { name: "To'lqin Jumayev", role: "Math Teacher", image: source1 },
+  { name: "Marhabo Xolmatova", role: "English Teacher", image: source2 },
+  { name: "Name Surname", role: "Science Teacher", image: source1 },
+  { name: "Anand Singh", role: "Music Teacher", image: source2 },
+  { name: "Name Surname", role: "Social Studies Teacher", image: source1 },
+  { name: "Name Surname", role: "Homeroom Teacher", image: source2 },
+  { name: "Name Surname", role: "School Nurse Teacher", image: source1 },
+  { name: "Name Surname", role: "ESL Teacher", image: source2 },
+  { name: "Name Surname", role: "Physical Education Teacher", image: source1 },
+  { name: "Name Surname", role: "Grade 2 Homeroom Teacher", image: source2 },
+];
+
+export default teamData;
