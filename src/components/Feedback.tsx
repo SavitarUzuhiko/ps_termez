35282f15501fba9b247451f8987d7711a856cadd
chingizpro <chingizprog@gmail.com>
2025-07-28 09:48:04 +0500

    Change feedback section design

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -1,8 +1,7 @@
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import "swiper/swiper-bundle.css";
-import { IoIosStar } from "react-icons/io";
+import feedbacks from "../data/feedbacks";
 
 const Feedback = () => {
   return (
@@ -25,142 +24,16 @@ const Feedback = () => {
             },
           }}
         >
-          <SwiperSlide>
-            <div className="feedback__outer">
-              <article className="feedback">
-                <h3 className="feedback__title">Lorem, ipsum.</h3>
-                <div className="feedback__rating">
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                </div>
-                <p className="feedback__text">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Ratione, id? Doloremque explicabo perferendis iure velit non
-                  ad praesentium nostrum commodi in dignissimos? Qui, fugiat.
-                </p>
-              </article>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="feedback__outer">
-              <article className="feedback">
-                <h3 className="feedback__title">Tempore, cum?</h3>
-                <div className="feedback__rating">
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                </div>
-                <p className="feedback__text">
-                  Et commodi assumenda expedita obcaecati, accusantium ipsa
-                  perferendis odio ea sunt? Dignissimos, sit. Libero a quo
-                  dolore minus vitae quibusdam eius fuga qui hic.
-                </p>
-              </article>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="feedback__outer">
-              <article className="feedback">
-                <h3 className="feedback__title">Minima, vitae.</h3>
-                <div className="feedback__rating">
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                </div>
-                <p className="feedback__text">
-                  Minus a laudantium eum nemo, sunt delectus necessitatibus
-                  dicta aspernatur eveniet. Delectus, ipsa, nihil illo natus
-                  voluptatem accusantium voluptatibus at sequi repellendus,
-                  optio recusandae!
-                </p>
-              </article>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="feedback__outer">
-              <article className="feedback">
-                <h3 className="feedback__title">Aperiam, officia!</h3>
-                <div className="feedback__rating">
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                </div>
-                <p className="feedback__text">
-                  Maxime animi facere voluptatem. Voluptatum architecto vero
-                  voluptate molestias rem cupiditate sequi iure blanditiis
-                  accusamus facere tempore aperiam voluptatibus, sint in ipsum
-                  accusantium? Explicabo!
-                </p>
-              </article>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="feedback__outer">
-              <article className="feedback">
-                <h3 className="feedback__title">Lorem, ipsum.</h3>
-                <div className="feedback__rating">
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                </div>
-                <p className="feedback__text">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Ratione, id? Doloremque explicabo perferendis iure velit non
-                  ad praesentium nostrum commodi in dignissimos? Qui, fugiat.
-                </p>
-              </article>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="feedback__outer">
-              <article className="feedback">
-                <h3 className="feedback__title">Tempore, cum?</h3>
-                <div className="feedback__rating">
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                </div>
-                <p className="feedback__text">
-                  Et commodi assumenda expedita obcaecati, accusantium ipsa
-                  perferendis odio ea sunt? Dignissimos, sit. Libero a quo
-                  dolore minus vitae quibusdam eius fuga qui hic.
-                </p>
-              </article>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="feedback__outer">
-              <article className="feedback">
-                <h3 className="feedback__title">Minima, vitae.</h3>
-                <div className="feedback__rating">
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                  <IoIosStar />
-                </div>
-                <p className="feedback__text">
-                  Minus a laudantium eum nemo, sunt delectus necessitatibus
-                  dicta aspernatur eveniet. Delectus, ipsa, nihil illo natus
-                  voluptatem accusantium voluptatibus at sequi repellendus,
-                  optio recusandae!
-                </p>
-              </article>
-            </div>
-          </SwiperSlide>
+          {feedbacks.map((f) => (
+            <SwiperSlide>
+              <div className="feedback__outer">
+                <article className="feedback">
+                  <h3 className="feedback__title">{f.name}</h3>
+                  <p className="feedback__text">{f.comment}</p>
+                </article>
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
---
+++ src/data/feedbacks.ts
@@ -0,0 +1,34 @@
+const feedbacks = [
+  {
+    name: "Name Surname",
+    comment:
+      "This school is great for the kids who want to find what their distinctive features. My son is studying here and he is also doing some sports. Everything is on spot",
+  },
+  {
+    name: "Norqobil Farhodov",
+    comment:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas, nam. Non culpa ipsa et quia sed officiis illo architecto aspernatur, porro esse error quis voluptatibus quos minus aperiam aliquid. Voluptatem.",
+  },
+  {
+    name: "Name Surname",
+    comment:
+      "This school is great for the kids who want to find what their distinctive features. My son is studying here and he is also doing some sports. Everything is on spot",
+  },
+  {
+    name: "Name Surname",
+    comment:
+      "This school is great for the kids who want to find what their distinctive features. My son is studying here and he is also doing some sports. Everything is on spot",
+  },
+  {
+    name: "Name Surname",
+    comment:
+      "This school is great for the kids who want to find what their distinctive features. My son is studying here and he is also doing some sports. Everything is on spot",
+  },
+  {
+    name: "Name Surname",
+    comment:
+      "This school is great for the kids who want to find what their distinctive features. My son is studying here and he is also doing some sports. Everything is on spot",
+  },
+];
+
+export default feedbacks;
