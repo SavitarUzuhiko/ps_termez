a6fe456890c85fbc68a9ec969554bc3a9638b7fa
chingizpro <chingizprog@gmail.com>
2025-07-28 11:10:16 +0500

    Redesign feedback slider

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -1,17 +1,48 @@
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 import feedbacks from "../data/feedbacks";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
+import { useState } from "react";
+
+function MyCustomPrevButton({ swiper }: { swiper: any }) {
+  return (
+    <button
+      onClick={() => swiper?.slidePrev()}
+      className="feedbacks__button feedbacks__button--prev"
+    >
+      <FaArrowLeft />
+    </button>
+  );
+}
+
+function MyCustomNextButton({ swiper }: { swiper: any }) {
+  return (
+    <button
+      onClick={() => swiper?.slideNext()}
+      className="feedbacks__button feedbacks__button--next"
+    >
+      <FaArrowRight />
+    </button>
+  );
+}
 
 const Feedback = () => {
+  const [swiperInstance, setSwiperInstance] = useState<any>(null);
+
   return (
     <div className="container">
       <div className="feedbacks__wrapper">
-        <h2 className="feedbacks__title">Feedback</h2>
+        <div className="feedbacks__header">
+          <h2 className="feedbacks__title">Feedback</h2>
+          <div className="feedbacks__buttons">
+            <MyCustomPrevButton swiper={swiperInstance} />
+            <MyCustomNextButton swiper={swiperInstance} />
+          </div>
+        </div>
+
         <Swiper
-          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          onSwiper={setSwiperInstance}
           slidesPerView={3}
-          navigation
           breakpoints={{
             0: {
               slidesPerView: 1,
@@ -24,8 +55,8 @@ const Feedback = () => {
             },
           }}
         >
-          {feedbacks.map((f) => (
-            <SwiperSlide>
+          {feedbacks.map((f, i) => (
+            <SwiperSlide key={f.name + i}>
               <div className="feedback__outer">
                 <article className="feedback">
                   <h3 className="feedback__title">{f.name}</h3>
